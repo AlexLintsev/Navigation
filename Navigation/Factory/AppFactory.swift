011bbc72bdd8168f7b926735624d98f9317faa0b
Alexander <alintsev@gmail.com>
2023-05-09 16:13:37 +0700

    Adding MVVM pattern

---
+++ Navigation/Factory/AppFactory.swift
@@ -0,0 +1,8 @@
+//
+//  AppFactory.swift
+//  Navigation
+//
+//  Created by Александр Линцев on 09.05.2023.
+//
+
+import Foundation
---
+++ Navigation/Modules/Module.swift
@@ -0,0 +1,8 @@
+//
+//  Module.swift
+//  Navigation
+//
+//  Created by Александр Линцев on 09.05.2023.
+//
+
+import Foundation
